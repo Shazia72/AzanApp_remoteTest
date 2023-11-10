74e1c6626af1119ff49bdf2718a2792f4c1eef0f
Shazia_Idrees <vohragee@gmail.com>
2023-11-10 10:37:35 -0500

    Initial Commit

---
+++ AzanApp/AzanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzanAppApp.swift
+//  AzanApp
+//
+//  Created by Shazia Vohra on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct AzanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzanApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AzanApp
+//
+//  Created by Shazia Vohra on 2023-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
